afccf5602901b080683e27f6af14db1228f4df67
satish <satishwaghmode2024@gmail.com>
2022-12-15 18:57:50 +0530

    done final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatmoduleModule } from './appModule/matmodule.module';
+import { HeaderComponent } from './includes/header/header.component';
+import { FooterComponent } from './includes/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { FormsModule } from '@angular/forms';
+import { CarouselPauseComponent } from './pages/carousel-pause/carousel-pause.component';
+       
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatmoduleModule,
+    NgbModule,
+    FormsModule,
+    CarouselPauseComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/appModule/matmodule.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { NgbDropdown, NgbDropdownMenu } from '@ng-bootstrap/ng-bootstrap';
+import { NgbOffcanvasConfig, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+
+        
+const material =[
+
+   MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSidenavModule,
+    NgbDropdown,
+    NgbDropdownMenu,
+    
+    
+   
+    
+]
+@NgModule({
+  declarations: [],
+ 
+  imports: [
+    CommonModule,
+    material
+    
+  ],
+
+  exports:[
+
+    material
+  ]
+})
+export class MatmoduleModule { }
